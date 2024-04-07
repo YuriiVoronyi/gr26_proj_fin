36dd953c4e529aa78828a1a732695f9dea428545
fogroy2000 <livetrade2000@gmail.com>
2024-04-07 16:57:23 +0200

    secconfig cleanup

--- src/main/java/de/aittr/gr26_proj_fin/config/SecurityConfig.java
+++ src/main/java/de/aittr/gr26_proj_fin/config/SecurityConfig.java
@@ -59,25 +59,8 @@ public class SecurityConfig {
 
                                 .requestMatchers(HttpMethod.DELETE, "/api/book/{name}").hasRole("ADMIN")//Удаление книги по названию
 
-//                                .requestMatchers(HttpMethod.GET, "/api/book/{name}").hasRole("ADMIN")//Вызов книги по названию
-//                                .requestMatchers(HttpMethod.GET, "/api/book/author/{author}").hasRole("ADMIN")//Вызов список книг по автору
-//                                .requestMatchers(HttpMethod.GET, "/api/book/genre/{genre}").hasRole("ADMIN")//Вызов список книг по жанру
-//                                .requestMatchers(HttpMethod.GET, "/api/book/year/{year}").hasRole("ADMIN")//Вызов список книг по году издания
-//                                .requestMatchers(HttpMethod.GET, "/api/book/isbn/{isbn}").hasRole("ADMIN")//Вызов список книг isbn
-
                                 .requestMatchers(HttpMethod.GET, "/api/book/forusername/active").permitAll()//Вывод всех активных книг
                                 .requestMatchers(HttpMethod.GET, "/api/book/forusername/{name}").permitAll()//Вывод активной книги по названию
-//                                .requestMatchers(HttpMethod.GET, "/api/book/foruserauthor/{author}").permitAll()//Вывод активной книги по автору
-//                                .requestMatchers(HttpMethod.GET, "/api/book/forusergenre/{genre}").permitAll()//Вывод активных книг по жанру
-//                                .requestMatchers(HttpMethod.GET, "/api/book/foruseryear/{year}").permitAll()//Вывод активных книг по году издания
-//                                .requestMatchers(HttpMethod.GET, "/api/book/foruserisbn/{isbn}").permitAll()//Вывод активной книги по isbn
-
-//                                .requestMatchers(HttpMethod.GET, "/api/user").hasRole("ADMIN")//Вывод всех юзеров
-//                                .requestMatchers(HttpMethod.GET, "/api/user/email/{email}").hasRole("ADMIN")//Вывод юзера по его почте
-//                                .requestMatchers(HttpMethod.GET, "/api/user/{name}").hasRole("ADMIN")//Вывод одного юзера
-//                                .requestMatchers(HttpMethod.DELETE, "/api/user/{name}").hasRole("ADMIN")//Удаление юзера по логину
-//                                .requestMatchers(HttpMethod.POST, "/api/user/update").hasRole("ADMIN")//Обновление данных по юзеру, логин и почта
-//                                .requestMatchers(HttpMethod.DELETE, "/api/user/delete/{id}").hasRole("ADMIN")//Удаление покупателя из базы данных
 
                                 .requestMatchers(HttpMethod.POST, "/api/user/reg/items").permitAll()//Регистрация юзера
 
